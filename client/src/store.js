5067ccf87902c256559f2a84beabc46b803f5ed3
Mona Lisa <george.v.kalashnikov@gmail.com>
2018-12-08 11:25:15 -0500

    updeate redux2

--- client/src/store.js
+++ client/src/store.js
@@ -1,25 +1,18 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
- 
+
 const initialState = {};
- 
+
 const middleware = [thunk];
- 
-// In development, use the browser's Redux dev tools extension if installed
-const enhancers = [];
-const isDevelopment = process.env.NODE_ENV === 'development';
-if (isDevelopment && typeof window !== 'undefined' && window.devToolsExtension) {
-  enhancers.push(window.devToolsExtension());
-}
- 
+
 const store = createStore(
   rootReducer,
   initialState,
   compose(
     applyMiddleware(...middleware),
-    ...enhancers,
-  ),
+    window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
- 
+
 export default store;
