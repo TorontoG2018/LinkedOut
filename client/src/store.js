25c7bbbe65f8797f26f296be978addd953b442bd
Mona Lisa <george.v.kalashnikov@gmail.com>
2018-12-08 13:22:19 -0500

    update8

--- client/src/store.js
+++ client/src/store.js
@@ -1,17 +1,24 @@
-import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import thunk from "redux-thunk";
-import rootReducer from "./reducers";
- 
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
 const initialState = {};
+
 const middleware = [thunk];
- 
-// Create the Store
-const store = createStore(
- rootReducer,
- initialState,
- composeWithDevTools(
- applyMiddleware(...middleware)
- // other store enhancers if any
- )
-);
+
+const composeEnhancer = process.env.NODE_ENV !== "production" &&
+window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    name: "App",
+    actionBlacklist: ["REDUX_STORAGE_SAVE"]
+  })
+: compose;
+
+const enhancer = composeEnhancer(applyMiddleware(...middleware));
+
+const store = createStore(rootReducer, initialState, enhancer);
+
+
+export default store;
+
+
